bd938b135a667bdb9e1304314fc022a77cdf8774
Klaus Weidinger <weidkl@gmx.de>
2026-04-23 21:05:22 +0200

    Fix assocs

--- app/models/sponsors/sponsorship.rb
+++ app/models/sponsors/sponsorship.rb
@@ -1,6 +1,6 @@
 class Sponsors::Sponsorship < ApplicationRecord
   include FriendlyId
   friendly_id :name
-  belongs_to :entities_organization
-  belongs_to :sponsors_sponsor_tier
+  belongs_to :entities_organization, class_name: "Entities::Organization"
+  belongs_to :sponsors_sponsor_tier, class_name: "Sponsors::SponsorTier"
 end
