25fc7e159556fe656d6789d2a9ea9d264dd8e7f0
Klaus Weidinger <weidkl@gmx.de>
2026-04-23 21:05:22 +0200

    Fix assocs

--- app/models/events/participation.rb
+++ app/models/events/participation.rb
@@ -1,4 +1,4 @@
 class Events::Participation < ApplicationRecord
-  belongs_to :entities_person
-  belongs_to :events_event
+  belongs_to :entities_person, class_name: "Entities::Person"
+  belongs_to :events_event, class_name: "Events::Event"
 end
--- app/models/talks/speaker_talk.rb
+++ app/models/talks/speaker_talk.rb
@@ -1,4 +1,4 @@
 class Talks::SpeakerTalk < ApplicationRecord
-  belongs_to :talks_talk
-  belongs_to :entities_person
+  belongs_to :talks_talk, class_name: "Talks::Talk"
+  belongs_to :entities_person, class_name: "Entities::Person"
 end
