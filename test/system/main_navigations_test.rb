6db6f6c3e8264ad398e40e05951a7be2b62b3727
Klaus Weidinger <weidkl@gmx.de>
2026-04-23 20:36:16 +0200

    bin/rails g system_test main_navigation

---
+++ test/system/main_navigations_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class MainNavigationsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit main_navigations_url
+  #
+  #   assert_selector "h1", text: "MainNavigation"
+  # end
+end
