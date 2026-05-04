d2152a57e1cf04b60516d379e525f3f22c4fc4c7
Klaus Weidinger <weidkl@gmx.de>
2026-05-04 18:22:25 +0200

    event->venue

--- app/models/events/event.rb
+++ app/models/events/event.rb
@@ -1,5 +1,6 @@
 class Events::Event < ApplicationRecord
   include FriendlyId
   friendly_id :title
-  belongs_to :venues_venue, class_name: "Venues::Venue"
+
+  belongs_to :venue, class_name: "Venues::Venue", foreign_key: :venues_venue_id, inverse_of: :events
 end
