d2600c6f661097cd7b86bbd5c0bbc6c87dcc720a
Klaus Weidinger <weidkl@gmx.de>
2026-05-04 18:57:37 +0200

    person <- speaker_talk -> talk

--- app/models/entities/person.rb
+++ app/models/entities/person.rb
@@ -4,6 +4,8 @@ class Entities::Person < ApplicationRecord
 
   has_many :participations, class_name: "Events::Participation", foreign_key: :entities_person_id, inverse_of: :person
   has_many :events, through: :participations, class_name: "Events::Event"
+  has_many :speaker_talks, class_name: "Talks::SpeakerTalk", foreign_key: :entities_person_id, inverse_of: :speaker
+  has_many :talks, class_name: "Talks::Talk", through: :speaker_talks
 
   def to_s
     name
