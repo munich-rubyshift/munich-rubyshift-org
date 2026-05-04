16fe760279b100d64e2bf9b8b460b703a8be28a4
Klaus Weidinger <weidkl@gmx.de>
2026-05-04 19:32:16 +0200

    sponsor_tier <- sponsorship -> organization

--- app/models/entities/organization.rb
+++ app/models/entities/organization.rb
@@ -1,4 +1,11 @@
 class Entities::Organization < ApplicationRecord
   include FriendlyId
   friendly_id :name
+
+  has_many :sponsorships, class_name: "Sponsors::Sponsorship", foreign_key: :entities_organization_id, inverse_of: :organization
+  has_many :sponsor_tiers, class_name: "Sponsors::SponsorTier", through: :sponsorships
+
+  def to_s
+    name
+  end
 end
--- app/models/sponsors/sponsor_tier.rb
+++ app/models/sponsors/sponsor_tier.rb
@@ -2,6 +2,8 @@ class Sponsors::SponsorTier < ApplicationRecord
   include FriendlyId
   friendly_id :name
   belongs_to :event, class_name: "Events::Event", foreign_key: :events_event_id, inverse_of: :sponsor_tiers
+  has_many :sponsorships, class_name: "Sponsors::Sponsorship", foreign_key: :sponsors_sponsor_tier_id, inverse_of: :sponsor_tier
+  has_many :organizations, class_name: "Entities::Organization", through: :sponsorships
 
   def to_s
     name
