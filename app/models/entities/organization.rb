ef7cb7d00188295b73ae41ee44751e43e4bef07a
Klaus Weidinger <weidkl@gmx.de>
2026-05-04 20:05:18 +0200

    format

--- app/models/entities/organization.rb
+++ app/models/entities/organization.rb
@@ -3,7 +3,6 @@ class Entities::Organization < ApplicationRecord
   friendly_id :name
 
   has_many :sponsorships, class_name: "Sponsors::Sponsorship", foreign_key: :entities_organization_id, inverse_of: :organization
-
   has_many :sponsor_tiers, class_name: "Sponsors::SponsorTier", through: :sponsorships
 
   def to_s
