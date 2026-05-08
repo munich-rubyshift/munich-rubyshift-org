dd16f79ff8f4fedc635cac6a57a0e1aebf92e6ff
Klaus Weidinger <32023517+dunkelziffer@users.noreply.github.com>
2026-05-08 20:20:30 +0200

    Upgrade static_db (#29)

--- config/initializers/static_db.rb
+++ config/initializers/static_db.rb
@@ -1,3 +1,7 @@
 StaticDb.configure do |config|
-  config.fixture_path = Rails.root.join("content", "data")
+  config.static_db_path = Rails.root.join("content", "data")
+
+  # Selectively activate loading and dumping. Active by default. Set to any other value to disable.
+  config.load = ENV["STATIC_DB"].in? [ "load", nil, "on", "true", "1" ]
+  config.dump = ENV["STATIC_DB"].in? [ "dump", nil, "on", "true", "1" ]
 end
